ad5f41e215d5b9cb2fd0800cbd12178ebffd800d
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-12-04 15:36:36 -0600

    Got rid of submodule

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
 int main(int argc, const char **argv)
 {
+    for (int i = 1; i < argc; i++)
+    {
+        // TODO
+    }
+    
     return 0;
 }
