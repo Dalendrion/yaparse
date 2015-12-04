bfa6f80272358cf0ec0bc2752c6f83cc5d3c4608
Gigabyte-Giant <brynden.bielefeld@hotmail.com>
2015-12-04 15:40:20 -0600

    Minor change

--- main.cpp
+++ main.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <string>
+#include "Lexer.hpp"
 using namespace std;
+
+vector<string> getFileLines(const char *filePath, char explodeOn)
+{
+    vector<string> lines;
+    
+    ifstream inputFile;
+    
+    inputFile.open(filePath);
+    
+    if (inputFile.is_open())
+    {
+        string tmpLn = "";
+        
+        char tmpChar;
+        
+        while (inputFile.get(tmpChar))
+        {
+            if (tmpChar != explodeOn)
+            {
+                tmpLn = tmpLn + tmpChar;
+            }
+            else
+            {
+                lines.push_back(tmpLn);
+                tmpLn = "";
+            }
+        }
+        
+        inputFile.close();
+    }
+    
+    return lines;
+}
 
 int main(int argc, const char **argv)
 {
+    Lexer myLex = Lexer();
+    
     for (int i = 1; i < argc; i++)
     {
-        // TODO
+        vector<string> fileLines = getFileLines(*(argv + i), '\n');
+        
+        for (size_t lnInd = 0; lnInd < fileLines.size(); lnInd++)
+        {
+            myLex.tokenize(fileLines.at(lnInd));
+        }
+        myLex.printList();
     }
     
     return 0;
